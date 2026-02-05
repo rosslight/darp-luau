639bec3f15475fbbaef7c3bfd09ff0c2bf72c82c
kpk <klape@freenet.de>
2026-02-05 14:05:54 +0100

    Initial. Fails to run due to "libc++_shared.so: cannot open shared object file: No such file or directory".

---
+++ examples/Darp.Luau.Examples.Unix/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Darp.Luau.Examples.Unix;
+
+internal sealed class Progarm
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        using var state = new LuauState();
+
+        Console.WriteLine("Hello world");
+    }
+}
