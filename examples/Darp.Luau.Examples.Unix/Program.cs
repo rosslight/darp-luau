324d40999e695225d1a5d8c39cb6812384b150b8
kpk <klape@freenet.de>
2026-02-06 16:32:24 +0100

    Example works now (needed to provide custom built libluau.so).

--- examples/Darp.Luau.Examples.Unix/Program.cs
+++ examples/Darp.Luau.Examples.Unix/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Darp.Luau.Examples.Unix;
+﻿using System.Text;
+
+namespace Darp.Luau.Examples.Unix;
 
 internal sealed class Progarm
 {
@@ -7,6 +9,25 @@ internal sealed class Progarm
     {
         using var state = new LuauState();
 
-        Console.WriteLine("Hello world");
+        LuauFunction funcLog = state.CreateFunctionBuilder((ref onCalled) =>
+        {
+            string strMsg = Encoding.UTF8.GetString(onCalled.CheckString(1));
+            Console.WriteLine(strMsg);
+        });
+        state.Globals.Set("log", funcLog);
+
+        state.DoString(
+            """
+            function add(a: number, b: number): ()
+                return a + b
+            end
+            
+            log("hello from luau")
+            """
+        );
+
+        _ = state.Globals.TryGet("add", out LuauFunction funcAdd);
+        double result = funcAdd.Call<double>(1, 2);        
+        Console.WriteLine(result);
     }
 }
