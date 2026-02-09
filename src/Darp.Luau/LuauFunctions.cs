8c1a139518a086a38b120043949e33c6435189ca
kpk <klape@freenet.de>
2026-02-09 08:01:04 +0100

    Added CheckBuffer and CheckBufferOrNil.

--- src/Darp.Luau/LuauFunctions.cs
+++ src/Darp.Luau/LuauFunctions.cs
@@ -163,6 +163,66 @@ public unsafe ref struct LuauFunctions
         return lua_tonumber(L, parameterIndex);
     }
 
+    /// <summary> Check if the parameter is a buffer and return it </summary>
+    /// <param name="parameterIndex"> The index of the parameter. 1 based </param>
+    /// <returns> The buffer bytes </returns>
+    /// <remarks> The resulting span points to lua owned memory! If a GC cycle is triggered this span might no longer be valid! </remarks>
+    public ReadOnlySpan<byte> CheckBuffer(int parameterIndex)
+    {
+        _state.ThrowIfDisposed();
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(parameterIndex, 0);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(parameterIndex, NumberOfParameters);
+        lua_State* L = _state.L;
+#if DEBUG
+        using var guard = new StackGuard(L, expectedDelta: 0);
+#endif
+        var parameterType = (lua_Type)lua_type(L, parameterIndex);
+        if (parameterType is not lua_Type.LUA_TBUFFER)
+        {
+            throw new ArgumentException(
+                $"Parameter {parameterIndex} must be {lua_Type.LUA_TBUFFER} but was {parameterType}."
+            );
+        }
+        
+        nuint nLength = 0;
+        void* pBuf = lua_tobuffer(L, -1, &nLength);
+        if (pBuf is null)
+            throw new ArgumentException($"Parameter {parameterIndex} returned a null pointer.");
+            
+        return new ReadOnlySpan<byte>(pBuf, (int)nLength);
+    }
+
+    public ReadOnlySpan<byte> CheckBufferOrNil(int parameterIndex, out bool isNull)
+    {
+        _state.ThrowIfDisposed();
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(parameterIndex, 0);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(parameterIndex, NumberOfParameters);
+        lua_State* L = _state.L;
+#if DEBUG
+        using var guard = new StackGuard(L, expectedDelta: 0);
+#endif
+        var parameterType = (lua_Type)lua_type(L, parameterIndex);
+        if (parameterType is lua_Type.LUA_TNIL)
+        {
+            isNull = true;
+            return null;
+        }
+        if (parameterType is not lua_Type.LUA_TBUFFER)
+        {
+            throw new ArgumentException(
+                $"Parameter {parameterIndex} must be {lua_Type.LUA_TBUFFER} but was {parameterType}."
+            );
+        }
+
+        nuint nLength = 0;
+        void* pBuf = lua_tobuffer(L, -1, &nLength);
+        if (pBuf is null)
+            throw new ArgumentException($"Parameter {parameterIndex} returned a null pointer.");
+
+        isNull = false;
+        return new ReadOnlySpan<byte>(pBuf, (int)nLength);
+    }
+
     public LuauValue CheckLuauValue(int parameterIndex) => throw new NotImplementedException();
 
     public LuauTable CheckLuauTable(int parameterIndex) => throw new NotImplementedException();
