889a71b91ee0184a57171d5cb7ba386f48521e30
kpk <klape@freenet.de>
2026-02-20 14:41:55 +0100

    Tests added.

---
+++ tests/Darp.Luau.Tests/RequireTests.cs
@@ -0,0 +1,41 @@
+using Shouldly;
+
+namespace Darp.Luau.Tests;
+
+public sealed class RequireTests
+{
+    private const string ScriptPath = "./scripts";
+
+    [Fact]
+    public void Result_in_globals()
+    {
+        using var state = new LuauState();
+        state.EnableRequire(ScriptPath);
+
+        state.DoString(File.ReadAllBytes(Path.Combine(ScriptPath, "main.luau")));
+        state.Globals.TryGet("result", out int nResult).ShouldBeTrue();
+        nResult.ShouldBe(15);
+    }
+
+    [Fact]
+    public void Results_as_return_values()
+    {
+        using var state = new LuauState();
+        state.EnableRequire(ScriptPath);
+
+        LuauValue[] results = state.DoString(File.ReadAllBytes(Path.Combine(ScriptPath, "main.luau")), nNumExpectedRetValues: 3);
+        
+        results[0].TryGet(out int nSum).ShouldBeTrue();
+        nSum.ShouldBe(3);
+        
+        results[1].TryGet(out int nDifference).ShouldBeTrue();
+        nDifference.ShouldBe(5);
+
+        results[2].TryGet(out LuauTable table).ShouldBeTrue();
+        table.TryGet("sum", out nSum).ShouldBeTrue();
+        nSum.ShouldBe(3);
+        table.TryGet("difference", out nDifference).ShouldBeTrue();
+        nDifference.ShouldBe(5);
+    }
+
+}
