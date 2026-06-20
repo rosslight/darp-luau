41cb7b9c129ed1a9cdecebb01f9f0d87a3089a1b
OleRoss <ole.rosskamp@rosslight.de>
2026-06-20 18:48:44 +0200

    fix: Require a name for the luauLibrary attribute

--- src/Darp.Luau/LuauLibraryAttribute.cs
+++ src/Darp.Luau/LuauLibraryAttribute.cs
@@ -6,11 +6,12 @@ public sealed class LuauLibraryAttribute : Attribute
 {
     /// <summary> Initializes a new instance of the <see cref="LuauLibraryAttribute"/> class. </summary>
     /// <param name="name">The Luau global name for the root library.</param>
-    public LuauLibraryAttribute(string? name = null)
+    public LuauLibraryAttribute(string name)
     {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
         Name = name;
     }
 
     /// <summary> Gets the Luau global name for the root library. </summary>
-    public string? Name { get; }
+    public string Name { get; }
 }
